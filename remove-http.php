fc1c4b58e02d359af1fcf88d05c400ee160c845d
Chris Wright <daverandom@php.net>
2017-03-04 16:01:51 +0000

    Use DOM-based replacement approach instead of regex

--- remove-http.php
+++ remove-http.php
@@ -12,9 +12,31 @@
 # If accessed directly, exit
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+class Fact_Maven_Remove_HTTP_Processing_Failed_Exception extends \Exception {}
+
 class Fact_Maven_Remove_HTTP {
 
     private $option;
+
+    # Keys are tag names, values are an array of attributes to process for those tags
+    private $replace_searches = array(
+        'script' => array( 'src' ),
+        'link' => array( 'href' ),
+        'base' => array( 'href' ),
+        'img' => array( 'src', 'srcset' ),
+        'form' => array( 'action' ),
+        'a' => array( 'href' ),
+        'meta' => array( 'content' ),
+        'iframe' => array( 'src' ),
+        'div' => array( 'data-project-file' ),
+        'svg' => array( 'data-project-file' ),
+    );
+
+    # These attributes will be processed for all tags
+    private $global_attrs = array( 'style' );
+
+    # The text content of these tags will be processed
+    private $global_content_tags = array( 'style' );
 
     public function __construct() {
         # Get plugin option
@@ -62,42 +84,81 @@ class Fact_Maven_Remove_HTTP {
         </fieldset> <?php
     }
 
-    private function get_content_type() {
-        # Try to find the value of the Content-Type header
+    private function is_applicable_content_type() {
+        # Try to find the value of the Content-Type header and check if it is text/html
         foreach ( headers_list() as $header ) {
             if ( stripos( $header, 'content-type:' ) === 0 ) {
                 $pieces = explode( ':', $header );
-                return strtolower( trim( $pieces[1] ) );
+                return strpos( strtolower( trim( $pieces[1] ) ), 'text/html' ) === 0;
             }
         }
-        # We didn't find it
-        return NULL;
+
+        # We didn't find it, it was not declared so assume HTML
+        return true;
     }
 
-    private function process_html( $html ) {
-        $tag = 'script|link|base|img|form|a|meta|iframe|svg|div';
-        $attribute = 'href|src|srcset|action|content|data-project-file';
-        # If 'Protocol Relative URL' option is checked, only apply change to internal links
-        if ( $this->option == 1 ) {
-            # Remove protocol from home URL
-            $website = preg_replace( '/https?:\/\//', '', home_url() );
-            # Remove protocol from internal links
-            $html = preg_replace( '/(<(' . $tag . ')([^>]*)(' . $attribute . ')=["\'])https?:\/\/' . $website . '/i', '$1//' . $website, $html );
+    private function process_element_attributes( DOMElement $element, array $attributes, $replace_regex ) {
+        foreach ( $attributes as $attribute ) {
+            # Check that the element actually has the target attribute
+            if ( !$element->hasAttribute( $attribute ) ) {
+                continue;
+            }
+
+            # Get the current attribute value, modify it and set it back on the element
+            $content = $element->getAttribute( $attribute );
+            $result = preg_replace( $replace_regex, '$1', $content );
+            $element->setAttribute( $attribute, $result );
         }
-        # Else, remove protocols from all links
-        else {
-            $html = preg_replace( '/(<(' . $tag . ')([^>]*)(' . $attribute . ')=["\'])https?:\/\//i', '$1//', $html );
+    }
+
+    private function process_html($html ) {
+        # Check that we have DOM loaded
+        if ( !class_exists( 'DOMDocument' ) || !class_exists( 'DOMXPath' ) ) {
+            throw new Fact_Maven_Remove_HTTP_Processing_Failed_Exception;
         }
-        # Return protocol relative links
-        return $html;
+
+        # Try to create a document and xpath
+        $doc = new \DOMDocument();
+        if ( !@$doc->loadHTML( $html, LIBXML_HTML_NODEFDTD ) ) {
+            throw new Fact_Maven_Remove_HTTP_Processing_Failed_Exception;
+        }
+        $xpath = new \DOMXPath( $doc );
+
+        # Create the regex in use based on the current option value
+        $replace_regex = $this->option == 1
+            ? '#https?:(//' . preg_replace( '#https?://#i', '', home_url() ) . ')#i'
+            : '#https?:(//[^/]+)#i';
+
+        # Process the specific tag lists first
+        foreach ( $this->replace_searches as $tag_name => $attributes ) {
+            $xpath_expr = '//' . $tag_name . '[@' . implode( ' or @', $attributes ) . ']';
+
+            foreach ( $xpath->query( $xpath_expr ) as $element ) {
+                $this->process_element_attributes( $element, $attributes, $replace_regex );
+            }
+        }
+
+        # Process global attributes
+        if ( !empty( $this->global_attrs ) ) {
+            foreach ( $xpath->query( '//*[@' . implode( ' or @', $this->global_attrs ) . ']' ) as $element ) {
+                $this->process_element_attributes( $element, $this->global_attrs, $replace_regex );
+            }
+        }
+
+        # Process global content elements
+        if ( !empty( $this->global_content_tags ) ) {
+            foreach ( $xpath->query( '//*[self::' . implode( ' or self::', $this->global_attrs ) . ']' ) as $element ) {
+                $element->textContent = preg_replace( $replace_regex, '$1', $element->textContent );
+            }
+        }
+
+        # Return the modified HTML as a string
+        return $doc->saveHTML();
     }
 
     public function ob_flush_handler( $html ) {
-        # Get the declared Content-Type, if any
-        $content_type = $this->get_content_type();
-
-        # Apply processing only if the type is declared as text/html or not declared
-        return $content_type === null || strpos( $content_type, 'text/html' ) === 0
+        # Apply processing only if the type is applicable
+        return $this->is_applicable_content_type()
             ? $this->process_html( $html )
             : $html;
     }
